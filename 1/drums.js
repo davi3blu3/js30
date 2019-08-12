7643b5a0cd2f9d896682e5c71d0c76e3332b4578
Dave Harned <dharned@gmail.com>
2019-08-12 15:34:08 -0500

    1 - setup and test drumkit keypresses

---
+++ 1/drums.js
@@ -0,0 +1,21 @@
+window.addEventListener('keydown', e => {
+  const keyEl = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  if (audio && keyEl) {
+    showPress(keyEl);
+    playAudio(audio);
+  }
+});
+
+const playAudio = audio => {
+  // audio.currentTime = 0;
+  // audio.play();
+};
+
+const showPress = el => {
+  if (!el) return;
+  el.classList.add('playing');
+  setTimeout(() => {
+    el.classList.remove('playing');
+  }, 200);
+};
